5ebf88ad3a3e6d3e63f0f510ae849973e5b8db03
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-10-02 17:26:50 -0300

    criacaoCssGeral

--- src/pages/tabelas/endereco/index.js
+++ src/pages/tabelas/endereco/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './style.css';
+import '../style/style-tabelas.css';
 
 function EnderecoHome() {
   const [clientes, setClientes] = useState([]);
